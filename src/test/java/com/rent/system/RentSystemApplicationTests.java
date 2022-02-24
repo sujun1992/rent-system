d57e43326c8f9653d9ceada7cf318892f78a672e
苏君 <jun.su@mobvoi.com>
2022-02-24 10:03:47 +0800

    创建工程

---
+++ src/test/java/com/rent/system/RentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rent.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
